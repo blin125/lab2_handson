ef7ab90e06cb60573dfffdc106cec401e1329f59
Bruno William <blin125@aucklanduni.ac.nz>
2022-08-07 18:42:17 +1200

    right_change

--- lab2_git.py
+++ lab2_git.py
@@ -73,8 +73,7 @@ def get_rand():
     """TODO: Add your code here 
         Hint: Using random.randint() function.
     """
-    my_randint = 0
-    return my_randint
+    return random.randint()
 
 
 def toss_coin():
